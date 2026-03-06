7da614009bafffb1d043e411c470e5de8f8f5799
georgedutygames-commits <georgealbuquerquetrabalho@gmail.com>
2026-03-06 14:52:55 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express")
+const mysql = require("mysql2")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+// conexão com banco
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1589",
+  database: "techstore"
+})
+
+// testar conexão
+db.connect((err)=>{
+  if(err){
+    console.log("Erro ao conectar:", err)
+  }else{
+    console.log("Conectado ao MySQL")
+  }
+})
+
+// rota login
+app.post("/login", (req,res)=>{
+
+  const {email, senha} = req.body
+
+  const sql = "SELECT * FROM usuarios WHERE email = ? AND senha = ?"
+
+  db.query(sql,[email,senha],(err,result)=>{
+
+    if(err){
+      return res.json({erro:true})
+    }
+
+    if(result.length > 0){
+      res.json({login:true})
+    }else{
+      res.json({login:false})
+    }
+
+  })
+
+})
+
+app.listen(3000, ()=>{
+  console.log("Servidor rodando na porta 3000")
+})
